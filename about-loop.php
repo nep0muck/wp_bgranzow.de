0fc8a353da6a32fd08af40963cacfe61883fe68d
nep0muck <me@eichlersolutions.de>
2016-05-02 11:36:14 +0200

    Added templatefiles for about page

---
+++ about-loop.php
@@ -0,0 +1,389 @@
+<section class="cards-vertical-2 row clearfix">
+              <div class="sitebrand-header blocks-about">
+                <img class="alignnone size-full wp-image-185 portrait" title="Benjamin Granzow" src="img/benni-portrait-199x199.png" alt="" width="199" height="199">
+                <h2 class="sidebar-brand">Benjamin Granzow</h2>
+                <p class="sub-title">Master of Sc. der Medieninformatik</p>
+              </div>
+            </section>
+
+            <section class="cards-vertical-2 row clearfix">
+              <div class="col-md-12 blocks-about desc">
+                <div class="col-md-2 text-right">
+                  <i class="glyphicon glyphicon-user"></i>
+                </div>
+                <div class="col-md-10">
+                  <p>Herzlich Willkommen, mein Name ist Benjamin Granzow und ich bin Student der Medieninformatik. Im Jahr 2009 habe ich mein Studium an der Beuth-Hochschule für Technik Berlin im Fachbereich Informatik und Medien angefangen und dadurch Erfahrungen in unterschiedlichen Bereichen wie Software-Entwicklung, Game-Design, Grafik-Design, Film und objektorientierter Programmierung gesammelt. Seit 2013 studiere ich im Medieninformatik Master mit dem Schwerpunkt Inter­ac­tive 3D.</p>
+
+                  <p>Da viele Projekte in Eigenarbeit erstellt werden mussten, habe ich die Fähigkeit erlangt, unbekannte Gebiete zu erschließen und Problemstellungen aus mehreren Perspektiven zu betrachten. Bei solch einem kreativen Prozess ergeben sich oft unkonventionelle Herangehensweisen.</p>
+
+                  <p>Besonderes Interesse habe ich an der Spielebranche, daher beobachte ich die Entwicklung dieses Bereiches stetig. Oft fragt man mich weswegen ich mich derart damit beschäftige. Es ist die Macht eines Computerspieles welche mich fasziniert, die theoretisch unbegrenzte Freiheit anderen eine Geschichte oder Begebenheit nahezubringen. Eine Umgebung zu schaffen welche einen fesselt.</p>
+                </div>
+              </div>
+            </section>
+
+            <section class="cards-vertical-2 row clearfix">
+              <div class="col-md-12 blocks-about skills">
+                <div class="col-md-2 text-right">
+                  <i class="glyphicon glyphicon-comment"></i>
+                </div>
+                <div class="col-md-10">
+                  <h2>Sprachen</h2>
+                  <ul>
+                    <li>
+                      Deutsch
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                          <span class="sr-only">Deutsch (Muttersprache)</span>
+                          <span class="skill-level">Experte</span>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      Englisch
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Englisch (Fortgeschritten)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-12 blocks-about skills">
+                <div class="col-md-2 text-right">
+                  <i class="glyphicon glyphicon-file"></i>
+                </div>
+                <div class="col-md-10">
+                  <h2>Anwendungen</h2>
+                  <ul>
+                    <li>
+                      Adobe CS
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Adobe CS (Grundkenntnisse)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Gimp
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Gimp (Grundkenntnisse)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Redmine
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Blender
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Git
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Experte</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Subversion
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Apache2
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Nginx
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Tomcat
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Twisted
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-12 blocks-about skills">
+                <div class="col-md-2 text-right">
+                  <i class="glyphicon glyphicon-cog"></i>
+                </div>
+                <div class="col-md-10">
+                  <h2>Betriebssysteme</h2>
+                  <ul>
+                    <li>
+                      Windows
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Linux(Debian)
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      MacOS X
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-12 blocks-about skills">
+                <div class="col-md-2 text-right">
+                  <i class="glyphicon glyphicon-edit"></i>
+                </div>
+                <div class="col-md-10">
+                  <h2>Programmiersprachen</h2>
+                  <ul>
+                    <li>
+                      C/C++
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      C#
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Java
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Python
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      MySQL
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-12 blocks-about skills">
+                <div class="col-md-2 text-right">
+                  <i class="glyphicon glyphicon-cloud"></i>
+                </div>
+                <div class="col-md-10">
+                  <h2>Technologien</h2>
+                  <ul>
+                    <li>
+                      Gradle
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      libavg
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Grundkenntnisse</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      OpenGL 4.5
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      OpelGL ES 3.2
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Qt 5.5
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Erfahren</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Shader Cg/GLSL
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Experte</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      Unity 3D
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li>
+                      WebGL 1.0
+                      <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+                          <span class="sr-only">Redmine (Erfahren)</span>
+                          <span class="skill-level">Fortgeschritten</span>
+                        </div>
+                      </div>
+                    </li>
+
+                  </ul>
+
+                </div>
+              </div>
+            </section>
+
+
+            <section class="cards-vertical-2 row clearfix">
+              <div class="col-md-12 blocks-about contact">
+                <div class="col-md-2 text-right">
+                  <i class="glyphicon glyphicon-envelope"></i>
+                </div>
+                <div class="col-md-10">
+                  <form>
+                    <div class="form-group">
+                      <label for="inputName">Name</label>
+                      <input type="text" class="form-control" id="inputName" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                      <label for="inputEmail1">E-Mail Addresse</label>
+                      <input type="email" class="form-control" id="inputEmail1" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                      <label for="inputSubject">Betreff</label>
+                      <select type="text" class="form-control" id="inputSubject" placeholder="Betreff...">
+                        <option selected>Jobangebot</option>
+                        <option>Fragen zu meiner Person</option>
+                        <option>Sonstiges</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputMessage">Nachricht</label>
+                      <textarea type="textarea" class="form-control" id="inputMessage" placeholder="Nachricht" rows="10"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-default">Abschicken</button>
+                  </form>
+                </div>
+              </div>
+            </section>
---
+++ template-about.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+  <main role="main">
+    <!-- section -->
+    <section>
+
+      <?php get_template_part('about-loop'); ?>
+
+      <?php get_template_part('pagination'); ?>
+
+    </section>
+    <!-- /section -->
+  </main>
+
+<?php /* get_sidebar(); */ ?>
+
+<?php get_footer(); ?>
