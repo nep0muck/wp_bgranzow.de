e2fce09a4009133331b4c020588e7c97e983a69b
nep0muck <me@eichlersolutions.de>
2016-05-02 13:15:29 +0200

    Cleaning

--- archive.php
+++ archive.php
@@ -4,8 +4,6 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
-
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
@@ -14,6 +12,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php /* get_sidebar(); */ ?>
 
 <?php get_footer(); ?>
