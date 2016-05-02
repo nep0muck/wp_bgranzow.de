e7b164d3c9b46ea71f8ff24a19a9215871b702b0
nep0muck <me@eichlersolutions.de>
2016-05-02 13:13:15 +0200

    Changed standard layout for categories

--- category.php
+++ category.php
@@ -4,8 +4,6 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
-
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
@@ -14,6 +12,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php /* get_sidebar(); */ ?>
 
 <?php get_footer(); ?>
