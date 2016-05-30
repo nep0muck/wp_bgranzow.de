81cfb5e108aa27dcee6ab3e3666ea513a7f5930f
nep0muck <me@eichlersolutions.de>
2016-05-31 00:03:11 +0200

    Additonal layout changes

--- functions.php
+++ functions.php
@@ -332,10 +332,13 @@ function html5blankcomments($comment, $args, $depth)
 	<?php if ( 'div' != $args['style'] ) : ?>
 	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
 	<?php endif; ?>
-    <div class="row">
-    	<div class="comment-author vcard col-md-6">
+    	<div class="comment-author vcard">
     	<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['180'] ); ?>
     	<?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()) ?>
+      <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?> class="text-right">
+        <?php
+          printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
+        ?>
     	</div>
     <?php if ($comment->comment_approved == '0') : ?>
     	<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
@@ -347,7 +350,6 @@ function html5blankcomments($comment, $args, $depth)
     			printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
     		?>
     	</div>
-    </div>
 
 	<?php comment_text() ?>
 
