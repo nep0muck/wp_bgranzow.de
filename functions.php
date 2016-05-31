c819c70e0792bcaa1c351a03a8f1c0ecfeeaf544
nep0muck <me@eichlersolutions.de>
2016-05-31 12:27:11 +0200

    Finalized comment styling

--- functions.php
+++ functions.php
@@ -332,31 +332,31 @@ function html5blankcomments($comment, $args, $depth)
 	<?php if ( 'div' != $args['style'] ) : ?>
 	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
 	<?php endif; ?>
-    <div class="row">
-    	<div class="comment-author vcard col-md-6">
+    	<div class="comment-author vcard">
     	<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['180'] ); ?>
     	<?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()) ?>
+        <span class="comment-meta-date"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
+        <?php
+            printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
+        ?></span>
     	</div>
     <?php if ($comment->comment_approved == '0') : ?>
     	<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
     	<br />
     <?php endif; ?>
 
-    	<div class="comment-meta commentmetadata col-md-6"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
-    		<?php
-    			printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
-    		?>
-    	</div>
-    </div>
-
 	<?php comment_text() ?>
 
 	<div class="reply">
 	<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
 	</div>
+
 	<?php if ( 'div' != $args['style'] ) : ?>
 	</div>
 	<?php endif; ?>
+
+    <hr>
+
 <?php }
 
 /*------------------------------------*\
